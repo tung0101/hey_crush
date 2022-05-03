d779b7cc04fd5fdde11449d872b03b4e787be8bd
tungunu <nguyendoantung12345@gmail.com>
2022-05-03 13:03:11 +0700

    crush

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: "Chào Cậu",
+    introDesc: `Tớ có điều này muốn hỏi Cậu, Cậu nhớ phải trả lời thật lòng nha`,
+    btnIntro: "OK",
+    title: "Em à anh thích em. Đồng ý làm người yêu anh nha ? 😙",
+    desc: "Em không trả lời hoặc thoát ra, tức là Em đồng ý rồi đó nha :> ",
+    btnYes: "Có, em đồng ý 💖 ",
+    btnNo: "Không đồng ý ! 💔 ",
+    question: "Nói cho Anh biết đi vì sao Em đồng ý 😊",
+    reasonPlaceholder: "Lý do",
+    btnReply: "Gởi ! 💌",
+    reply: "Tại vì em cũng yêu anh 😊",
+    mess: "Anh biết mà, Anh cũng yêu em lắm 🥰.I Love you  😘",
+    messDesc: "💬💬 Nhắn tin cho Anh nha. 💬💬",
+    btnAccept: "Ok lun 💖",
+    messLink: "https://www.messenger.com/t/100033312727449/",
+};
